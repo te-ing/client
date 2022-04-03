26d0de3961e63c951794f0c5946480d4bf8f70cd
choi2601 <byeonghyeonchoe55@gmail.com>
2022-04-03 14:00:33 +0900

    ✨ SetUserProfile 세팅 SetUserProfile 컴포넌트 초기 세팅 #7

---
+++ components/initialRegister/setUserInterest/SetUserInterest.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import * as S from './SetUserInterest.style';
+
+const SetUserInterest: React.FC = () => {
+    return (
+        <S.Wrapper>
+            SetUserInterest
+        </S.Wrapper>
+    )
+}
+
+export default SetUserInterest;
