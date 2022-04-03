276a3857d49e3fecd95d915f58b4163ee787288c
choi2601 <byeonghyeonchoe55@gmail.com>
2022-04-03 15:17:06 +0900

    🛠  SetUserProfile  세팅 수정 SetUserProfile merge 후 재세팅 #7

---
+++ components/initialRegister/setUserProfile/SetUserProfile.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import * as S from './SetUserProfile.style';
+
+const SetUserProfile: React.FC = () => {
+    return (
+        <S.Wrapper>
+            SetUserProfile 컴포넌트
+        </S.Wrapper>
+    )
+}
+
+export default SetUserProfile;
