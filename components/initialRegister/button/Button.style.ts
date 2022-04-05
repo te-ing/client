ab26c753c16c9b9d524d0724f484149ee066a538
choi2601 <byeonghyeonchoe55@gmail.com>
2022-04-05 17:15:15 +0900

    🛠  InitialRegister Button style 수정 #14

--- components/initialRegister/button/Button.style.ts
+++ components/initialRegister/button/Button.style.ts
@@ -2,9 +2,9 @@ import styled from 'styled-components';
 import type { StyledButtonPropsType } from './Button.type';
 
 export const StyledButton = styled.button<StyledButtonPropsType>`
-    width: ${({ sort }) => sort === 'setUserProfile' ? '520px' : (sort === 'setUserInterest' ? '552px' : '368px')};
-    height: 56px;
-    padding: 16px 165px;
+    min-width: ${({ sort }) => sort === 'setUserProfile' ? '520px' : (sort === 'setUserInterest' ? '552px' : '368px')};
+    min-height: 56px;
+    padding: 16px ${({ sort }) => sort === 'setUserProfile' ? '165px' : (sort === 'setUserInterest' ? '197px' : '116.5px')};
     border: none;
     border-radius: 4px;
     font-size: 20px;
