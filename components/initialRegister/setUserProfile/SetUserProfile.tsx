b2391e1d8a3e986ae31e653b27d910ffa885b223
choi2601 <byeonghyeonchoe55@gmail.com>
2022-04-05 18:57:20 +0900

    🛠  svg component hotfix custom.d.ts 파일 추가 #7

--- components/initialRegister/setUserProfile/SetUserProfile.tsx
+++ components/initialRegister/setUserProfile/SetUserProfile.tsx
@@ -1,10 +1,18 @@
 import React from 'react';
 import * as S from './SetUserProfile.style';
 
+import { ReactComponent as ProfileIcon } from '/images/icon-profile.svg';
+
 const SetUserProfile: React.FC = () => {
     return (
         <S.Wrapper>
-            SetUserProfile 컴포넌트
+            <S.InfoHeader>
+                <S.Title>프로필을 설정 해주세요</S.Title>
+                <S.SubInfo>다양한 작품, 작업물을 올리기 전에 <br /> 나의 관심사를 설정해 두면 도움이 됩니다.</S.SubInfo>
+            </S.InfoHeader>
+            <S.ProfileWrapper>
+
+            </S.ProfileWrapper>
         </S.Wrapper>
     )
 }
