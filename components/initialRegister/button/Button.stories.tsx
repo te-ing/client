b6f7da82affd7a4d5efa4fb8d9ab7b9354a58ae1
choi2601 <byeonghyeonchoe55@gmail.com>
2022-04-05 18:09:32 +0900

    ✨ InitialRegister Button stories 적용 #14

---
+++ components/initialRegister/button/Button.stories.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import Button from './Button';
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default {
+    component: Button,
+    title: 'initialRegister_Button'
+}
+
+const Template = args => <Button {...args}/>
+
+export const SetUserProfile = Template.bind({});
+SetUserProfile.args = {
+    sort: 'setUserProfile',
+    name: '관심분야 설정하러가기'
+}
+
+export const SetUserInterest = Template.bind({});
+SetUserInterest.args = {
+    sort: 'setUserInterest',
+    name: '관심분야 설정 완료'
+}
+
+export const CompleteRegister = Template.bind({});
+CompleteRegister.args = {
+    sort: 'setUserComplete',
+    name: '프로필 설정하기'
+}
