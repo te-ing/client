0f6457a679fa9e2fedd9b96e966286c0607ef088
choi2601 <byeonghyeonchoe55@gmail.com>
2022-04-05 19:22:25 +0900

    ✨ SetUserProfile 마크업 초기 구성 #7

--- components/initialRegister/setUserProfile/SetUserProfile.tsx
+++ components/initialRegister/setUserProfile/SetUserProfile.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import * as S from './SetUserProfile.style';
 
+import Button from '../button/Button';
 import { ReactComponent as ProfileIcon } from '/images/icon-profile.svg';
+import { ReactComponent as CameraIcon } from '/images/icon-camera.svg';
 
 const SetUserProfile: React.FC = () => {
     return (
@@ -11,8 +13,23 @@ const SetUserProfile: React.FC = () => {
                 <S.SubInfo>다양한 작품, 작업물을 올리기 전에 <br /> 나의 관심사를 설정해 두면 도움이 됩니다.</S.SubInfo>
             </S.InfoHeader>
             <S.ProfileWrapper>
-
+                <ProfileIcon />
+                <CameraIconWraper>
+                    <CameraIcon />
+                </CameraIconWraper>
             </S.ProfileWrapper>
+            <S.UserInfoInputWrapper>
+                <UserInfoInputInner>
+                    <InfoLabel>이메일</InfoLabel>
+                    <UserInfoInput />
+                </UserInfoInputInner>
+                <UserInfoInputInner>
+                    <InfoLabel>이메일</InfoLabel>
+                    <UserInfoInput />
+                </UserInfoInputInner>
+            </S.UserInfoInputWrapper>
+            <Button />
+            <SkipButtton />
         </S.Wrapper>
     )
 }
