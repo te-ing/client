04d8d00a605802cc703e13eebf531479aa6c296d
choi2601 <byeonghyeonchoe55@gmail.com>
2022-04-17 02:22:26 +0900

    ✨ auth recoil 초기 세팅 #7

--- hooks/useImageHandle.ts
+++ hooks/useImageHandle.ts
@@ -1,9 +1,12 @@
 import React, { useState } from 'react';
+import { useRecoilState } from 'recoil';
+
+import { userRegisterInfoState } from '../recoil/auth';
 
 import { readFile } from '../utils/readFile';
 
 const useImageHandle = () => {
-    
+    const [userInfo, setUserInfo] = useRecoilState(userRegisterInfoState);
 
     const storeImage = async (e: React.ChangeEvent<HTMLInputElement>) => {
         const currentFile = (e.target as HTMLInputElement).files[0];
---
+++ recoil/auth.ts
@@ -0,0 +1,8 @@
+import { atom } from 'recoil';
+
+// export interface UserRegisterInfoType {}
+
+export const userRegisterInfoState = atom({
+    key: 'userRegisterInfoState',
+    default: {}
+})
