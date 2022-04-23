00c784886b43c82727932f41e23bcc91b641b5dd
choi2601 <byeonghyeonchoe55@gmail.com>
2022-04-23 23:45:11 +0900

    💄 CompleteRegister 스타일링 #26

--- components/initialRegister/completeRegister/CompleteRegister.tsx
+++ components/initialRegister/completeRegister/CompleteRegister.tsx
@@ -1,10 +1,18 @@
 import React from 'react';
 import * as S from './CompleteRegitser.style';
 
+import Button from '../button/Button';
 
 const CompleteRegister = () => {
     return (
-        <S.Wrapper>CompleteRegister</S.Wrapper>
+        <S.Wrapper>
+            <S.Header>DREAM IN</S.Header>
+            <S.InfoWrapper>
+                <S.Info>가입 성공!</S.Info>
+                <S.SubInfo>드림인에서 다양한 작품을 업로드하고 <br /> 프로필을 설정하여 자신을 소개해 보세요!</S.SubInfo>
+            </S.InfoWrapper>
+            <Button sort="setUserComplete" name="프로필 설정하기"/>
+        </S.Wrapper>
     )
 }
 
