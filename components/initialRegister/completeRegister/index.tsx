7244298619bf4bdd1d7072d035edc329d1b3d8b5
Byeong Hyeon <byeonghyeonchoe55@gmail.com>
2022-04-25 23:10:39 +0900

    Delete components/initialRegister/completeRegister directory

--- components/initialRegister/completeRegister/index.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import * as S from './CompleteRegitser.style';
-
-const CompleteRegister = () => {
-  return <S.Wrapper>CompleteRegister</S.Wrapper>;
-};
-
-export default CompleteRegister;
