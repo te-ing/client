a1c8aa1f7fbdf4e0863accc54b696272e1e4950c
Byeong Hyeon <byeonghyeonchoe55@gmail.com>
2022-04-26 00:05:06 +0900

    Delete components directory

--- components/initialRegister/SetUserInterest/index.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import * as S from './SetUserInterest.style';
-
-const SetUserInterest: React.FC = () => {
-  return <S.Wrapper>SetUserInterest</S.Wrapper>;
-};
-
-export default SetUserInterest;
