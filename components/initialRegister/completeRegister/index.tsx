74f4feeb830d2dd8b201cd9dde227da0e6d8adad
Byeong Hyeon <byeonghyeonchoe55@gmail.com>
2022-04-26 01:14:17 +0900

    Delete index.tsx

--- components/initialRegister/completeRegister/index.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import * as S from './CompleteRegister.style';
-
-const CompleteRegister = () => {
-  return <S.Wrapper>CompleteRegister</S.Wrapper>;
-};
-
-export default CompleteRegister;
