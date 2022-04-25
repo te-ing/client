8f796362f7e23a88d129b0c95985a8e6232f05d4
choi2601 <byeonghyeonchoe55@gmail.com>
2022-04-26 01:29:37 +0900

    ✅ conflict solved #26

--- components/common/modal/Modal.type.ts
+++
@@ -1,4 +0,0 @@
-export interface ModalPropsType {
-    isShowing: boolean;
-    hide: (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => void;
-}
--- components/initialRegister/button/Button.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import * as S from './Button.style';
-import type { ButtonPropsType } from './Button.type';
-
-const Button: React.FC<ButtonPropsType> = ({ sort, name, navigateToNext }) => <S.StyledButton sort={sort} name={name} onClick={navigateToNext}>{name}</S.StyledButton>
-
-
-export default Button;
