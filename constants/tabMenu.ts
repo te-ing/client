4809063f1f2b39ffd535dd60182374066394c377
abg3000 <abg3000@naver.com>
2022-04-28 19:21:11 +0900

    ♻ 탭 메뉴 코드 리팩토링

---
+++ constants/tabMenu.ts
@@ -0,0 +1,4 @@
+export const tabMenuArr = [
+  { id: 'post', name: '작업물', isActive: true },
+  { id: 'scrap', name: '스크랩', isActive: false },
+];
--- pages/profile.tsx
+++ pages/profile.tsx
@@ -4,13 +4,15 @@ import Banner from 'components/Profile/Banner';
 import UserInfo from 'components/Profile/UserInfo';
 import ItemList from 'components/Profile/ItemList';
 import { TabButton } from 'components/common/Atomic/Tabs/TabButton';
+import { tabMenuArr } from 'constants/tabMenu';
+
 const Profile: React.FC = () => {
   const [user, getUserQuery] = useState('serre'); // useQuery로 유저정보 받아옴.
 
   //Suspense를 사용하게 된다면, useQuery를 여러개 선언하는것은 사용할 수 없으므로, useQueries를 사용해야함
   const Items = {
-    items: [], //['아이템1', '아이템2'],
-    scrabs: [
+    post: ['작업물1'], //['아이템1', '아이템2'],
+    scrap: [
       '',
       '스크랩1 제목입니다.스크랩1 제목입니다.스크랩1 제목입니다.스크랩1 제목입니다.스크랩1 제목입니다.',
       '스크랩2',
@@ -20,32 +22,38 @@ const Profile: React.FC = () => {
       '스크랩6',
       '스크랩7',
       '스크랩8',
+      '스크랩9',
     ],
   };
-  const [currentTab, setCurrentTab] = useState(0);
-  const tabMenuArr = [
-    ['작업물', 'items'],
-    ['스크랩', 'scrabs'],
-  ];
+  const [currentTab, setCurrentTab] = useState('post');
+
   const selectTab = useCallback(
-    (index: number) => () => {
-      setCurrentTab(index);
+    (id: string) => () => {
+      tabMenuArr.forEach((tab) => {
+        if (tab.id === id) {
+          tab.isActive = true;
+          setCurrentTab(tab.id);
+        } else {
+          tab.isActive = false;
+        }
+      });
     },
-    [currentTab, setCurrentTab]
+    [currentTab]
   );
+
   return (
     <Layout>
       <Banner />
       <UserInfo />
       <div style={{ marginBottom: '40px' }}>
         {tabMenuArr.map((tab, i) => (
-          <TabButton active={currentTab === i} key={i} onClick={selectTab(i)}>
-            {tab[0]}
-            <span>{Items[tabMenuArr[i][1]].length}</span>
+          <TabButton active={tab.isActive} key={i} onClick={selectTab(tab.id)}>
+            {tab.name}
+            <span>{Items[tab.id].length}</span>
           </TabButton>
         ))}
       </div>
-      <ItemList itemList={Items[tabMenuArr[currentTab][1]]} />
+      <ItemList itemList={Items[currentTab]} />
     </Layout>
   );
 };
