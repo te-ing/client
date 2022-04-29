4070b70fa73f98b14c3828f186ce5b74743d38ca
abg3000 <abg3000@naver.com>
2022-04-29 17:53:36 +0900

    ♻ 이미지 경로 변수화

---
+++ constants/imgUrl.ts
@@ -0,0 +1,5 @@
+export const default_banner = '/images/add_project_default.svg';
+
+export const default_profile = '/images/icon-profile.svg';
+
+export const camera_icon = '/images/icon-camera.svg';
