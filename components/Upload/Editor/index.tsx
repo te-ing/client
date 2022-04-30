4a31c09d669ffe0e4f5ff13555af3b46a45fdcb5
choi2601 <byeonghyeonchoe55@gmail.com>
2022-04-30 18:06:09 +0900

    💄 quill editor 스타일링 #40

--- components/Upload/Editor/index.tsx
+++ components/Upload/Editor/index.tsx
@@ -1,35 +1,37 @@
-import React, { useEffect, useRef } from 'react';
+import React from 'react';
 import * as S from './Editor.style';
 
 import dynamic from 'next/dynamic';
+const Quill = dynamic(import('react-quill'), { ssr: false });
 
-import Quill = dynamic(() => import("quill"), { ssr: false });;
-import 'quill/dist/quill.bubble.css';
+import 'react-quill/dist/quill.bubble.css';
+
+const modules = {
+  toolbar: {
+    container: [
+      [{ header: [1, 2, 3, 4, 5, 6, false] }],
+      [{ align: [] }],
+      ['bold', 'italic', 'underline', 'strike', 'blockquote'],
+      [{ list: 'ordered' }, { list: 'bullet' }, 'link'],
+    ],
+  },
+};
+
+const formats = ['header', 'align', 'bold', 'italic', 'underline', 'strike', 'blockquote', 'list', 'bullet', 'link'];
 
 const Editor: React.FC = () => {
-  const quillElement = useRef(null);
-  const quillInstance = useRef(null);
-
-  useEffect(() => {
-    quillInstance.current = new Quill(quillElement.current, {
-      theme: 'bubble',
-      placeholder: '작품에 대한 설명을 적어주세요',
-      modules: {
-        toolbar: [
-          [{ header: '1' }, { header: '2' }],
-          ['bold', 'italic', 'underline', 'strike'],
-          [{ list: 'ordered' }, { list: 'bullet' }],
-          ['blockquote', 'code-block', 'link'],
-        ],
-      },
-    });
-  }, []);
-
   return (
     <S.Wrapper>
-      <S.QuillWrapper>
-        <S.Quill ref={quillElement} />
-      </S.QuillWrapper>
+      <S.EditorWrapper>
+        <S.TitleInput placeholder="제목을 입력해주세요" />
+        <Quill
+          style={{ height: '175px', border: 'none', background: '#F5F5F5' }}
+          modules={modules}
+          formats={formats}
+          theme="bubble"
+          placeholder="작품에 대한 설명을 적어주세요"
+        />
+      </S.EditorWrapper>
     </S.Wrapper>
   );
 };
