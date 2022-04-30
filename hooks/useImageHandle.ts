ba1478a4d8ab7e1b18a0fd42fd4db8383ff66acb
choi2601 <byeonghyeonchoe55@gmail.com>
2022-04-30 16:07:32 +0900

    🛠  S3 이미지 업로드 기능 수정 #37

--- hooks/useImageHandle.ts
+++ hooks/useImageHandle.ts
@@ -6,37 +6,48 @@ import { useRecoilState } from 'recoil';
 import { userRegisterInfoState } from 'recoil/auth';
 import type { UserRegisterInfoType } from 'recoil/auth';
 
-const useImageHandle = () => {
-  const [userInfo, setUserInfo] = useRecoilState<UserRegisterInfoType>(userRegisterInfoState);
+const useImageHandle = (name: string) => {
+  const [info, setInfo] = (function fetchStore(name) {
+    switch (name) {
+      case 'profile':
+        return useRecoilState<UserRegisterInfoType>(userRegisterInfoState);
+    }
+  })(name);
   const [isUpload, setIsUpload] = useState<boolean>(false);
 
   AWS.config.update({
-    region: process.env.AWS_REGION,
-    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+    accessKeyId: process.env.NEXT_PUBLIC_AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.NEXT_PUBLIC_AWS_SECRET_ACCESS_KEY,
+  });
+
+  const myBucket = new AWS.S3({
+    params: { Bucket: process.env.NEXT_PUBLIC_AWS_STORAGE_BUCKET_NAME },
+    region: process.env.NEXT_PUBLIC_AWS_REGION,
   });
 
   const storeImage = async (e: React.ChangeEvent<HTMLInputElement>) => {
     const currentFile = (e.target as HTMLInputElement).files[0];
+    const currentFileName = currentFile.name.replaceAll(' ', '');
+
+    const currentFileExtension = currentFileName.split('.')[1];
+
+    if (currentFileExtension !== ('png' || 'jpg' || 'jpeg')) return;
 
     const params = {
       ACL: 'public-read',
       Body: currentFile,
-      Bucket: process.env.AWS_STORAGE_BUCKET_NAME,
-      Key: `upload/${currentFile.name}`,
+      Bucket: process.env.NEXT_PUBLIC_AWS_STORAGE_BUCKET_NAME,
+      Key: `${name}/${currentFileName}`,
+      ContentType: `text/${currentFileExtension}`, // text/plain
     };
 
-    const upload = new AWS.S3.ManagedUpload({
-      params: params,
-    });
-
-    upload
-      .promise()
-      .then(() => {
-        alert('image upload success!');
+    myBucket
+      .putObject(params)
+      .on('httpUploadProgress', (Progress, Response) => {
+        console.log(Response);
       })
-      .catch((e) => {
-        alert(`occur error: ${e.message}`);
+      .send((err) => {
+        if (err) console.log(err);
       });
 
     setIsUpload(true);
