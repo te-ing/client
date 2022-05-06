79fe62caab818af9cfa69275b4503380f8820ca3
Yena-Yun <cheryleduart@gmail.com>
2022-05-06 14:14:04 +0900

    :wrench: 절대경로 설정

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 
-import Modal from '../components/common/Modal';
-import ModalTemplate from '../components/common/Modal/ModalTemplate';
-import SetUserProfile from '../components/initialRegister/SetUserProfile';
+import Modal from 'components/common/Modal';
+import ModalTemplate from 'components/common/Modal/ModalTemplate';
+import SetUserProfile from 'components/initialRegister/SetUserProfile';
 
-import useModal from '../hooks/useModal';
+import useModal from 'hooks/useModal';
 
 const Index: React.FC = () => {
   const { isShowing, setModalVisible } = useModal();
