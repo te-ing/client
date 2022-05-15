af33fd1017a981e33ab9714d740c19395477619b
Yena-Yun <cheryleduart@gmail.com>
2022-05-15 14:00:46 +0900

    :construction: react-aws-s3로 s3 업로드 구현하던 코드와 fs 에러 수정하던 코드 여기 브랜치에 잠시 저장

--- components/Upload/Register/index.tsx
+++ components/Upload/Register/index.tsx
@@ -1,4 +1,8 @@
 import * as S from './Register.style';
+import S3 from 'react-aws-s3';
+import { v4 as uuidv4 } from 'uuid';
+import dotenv from 'dotenv';
+// import axios from axios;
 import ImageUploadWrapper from 'components/common/ImageUploadWrapper';
 import Image from 'next/image';
 import { useRef, useState } from 'react';
@@ -6,19 +10,58 @@ import { default_profile } from 'constants/imgUrl';
 
 const contents = ['image', 'edit', 'video'];
 
+dotenv.config();
+
 const Register = () => {
   const [imgFile, setImgFile] = useState(null);
+  const [previewImage, setPreviewImage] = useState(null);
   const inputOpenImageRef = useRef(null);
+
+  // const imagePatchConfig = {
+  //   headers: {
+  //     Authorization: `Bearer ${JSON.parse(localStorage.getItem('userInfo')).token}`,
+  //     'Content-Type': 'application/json',
+  //   },
+  // };
 
   const handleChangeFile = (e) => {
     const file = e.target.files[0];
+    setImgFile(file);
 
     // 로컬에 미리보기 렌더링
     const reader = new FileReader();
     reader.readAsDataURL(file);
     reader.onload = (e) => {
-      setImgFile(e.target.result);
+      setPreviewImage(e.target.result);
     };
+  };
+
+  // S3에 최종 선택한 이미지 업로드
+  const handleUpload = (e) => {
+    const newFileName = uuidv4();
+
+    const config = {
+      bucketName: process.env.AWS_STORAGE_BUCKET_NAME,
+      region: process.env.AWS_REGION,
+      accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+      secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+    };
+
+    const ReactS3Client = new S3(config);
+    console.log(imgFile.type);
+    console.log(imgFile);
+
+    if (imgFile) {
+      if (imgFile.type === 'image/jpeg' || imgFile.type === 'image/png' || imgFile.type === 'image/jpg') {
+        console.log(imgFile);
+        ReactS3Client.uploadFile(imgFile, newFileName).then((data) => {
+          console.log(data);
+        });
+      } else {
+        alert('JPEG, PNG, JPG 파일만 업로드 가능합니다.');
+        e.target.value = null;
+      }
+    }
   };
 
   const handleOpenImageRef = () => {
@@ -29,7 +72,7 @@ const Register = () => {
     <S.Wrapper>
       {imgFile ? (
         <S.ImageContainer>
-          <Image src={imgFile ? imgFile : default_profile} width="300" height="300" />
+          <Image src={previewImage ? previewImage : default_profile} width="300" height="300" />
         </S.ImageContainer>
       ) : (
         <>
@@ -67,7 +110,8 @@ const Register = () => {
           </S.RegisterButtonWrapper>
         </>
       )}
-      <S.UploadButton>업로드 하기</S.UploadButton>
+      <S.UploadButton onClick={(e) => handleUpload(e)}>업로드 하기</S.UploadButton>
+      {/* <S.UploadButton>업로드 하기</S.UploadButton> */}
     </S.Wrapper>
   );
 };
