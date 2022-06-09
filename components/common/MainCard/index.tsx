4a94beedc066f76fd7c30dfce153b7f689359a5f
Taejung Kim <hi2177@naver.com>
2022-06-09 10:14:05 +0900

    ✨ 메인페이지 포스트카드 레이아웃 구현

--- components/common/MainCard/index.tsx
+++ components/common/MainCard/index.tsx
@@ -1,28 +1,59 @@
+import usersApi from 'apis/users.api';
 import Image from 'next/image';
+import { useRouter } from 'next/router';
+import { useQuery } from 'react-query';
 import styled from 'styled-components';
 import { FlexBox, FlexCenter, FlexColumn, TextBox } from 'styles/commonStyles';
+import { PostType } from 'types/post';
+import { User } from 'types/user';
+interface Props {
+  post: PostType;
+}
 
-const MainCard = () => {
+const MainCard = ({ post }: Props) => {
+  const getAuthor = async () => {
+    const data = await usersApi.getUserInfo(post.author);
+    return data;
+  };
+  const { data } = useQuery<User>('user', getAuthor);
+  const router = useRouter();
+  const userData = data;
+
   return (
     <Wrapper>
-      <PreviewImageBox>
-        <Image src={'/images/logo.svg'} width="100%" height="100%" />
-        <FolderAddIcon>
-          <Image src={'/images/folder.svg'} width="24" height="24" />
-        </FolderAddIcon>
+      <PreviewImageBox onClick={() => router.push(`/post/${post.id}`)}>
+        <Image src={post.images[0].image || '/images/logo.svg'} layout="fill" objectFit="cover" />
+        <PreviewImageBoxLayer />
+        <PostTitle>{post.title}</PostTitle>
       </PreviewImageBox>
       <CardInfo>
-        <FlexCenter>
+        <InfoUserBox onClick={() => router.push(`/user/${post.author}`)}>
           <ProfileImageBox>
-            <Image src={'/images/icon-profile.svg'} width={32} height={32} />
+            <Image src={userData?.profileImage || '/images/icon-profile.svg'} width={32} height={32} />
           </ProfileImageBox>
           <TextBox size="20" weight={600}>
-            작업자 이름
+            {userData?.nickname}
           </TextBox>
-        </FlexCenter>
+        </InfoUserBox>
         <FlexBox>
-          <FlexBox style={{ cursor: 'pointer' }}>{/* LikesIcon */}5</FlexBox>
-          <FlexBox style={{ cursor: 'pointer' }}>{/* BookmarkIcon */}5</FlexBox>
+          <InfoImageBox>
+            <Image
+              alt="like"
+              src={post.isLike ? '/images/like-small.svg' : '/images/like-border-small.svg'}
+              width="16px"
+              height="16px"
+            />
+            <CountInfo>{post.likeCount}</CountInfo>
+          </InfoImageBox>
+          <InfoImageBox>
+            <Image
+              alt="scrap"
+              src={post.isScrap ? '/images/scrap-small.svg' : '/images/scrap-border-small.svg'}
+              width="16px"
+              height="16px"
+            />
+            <CountInfo>{post.commentCount /* scrapCount 대신 임시로 commentCount 사용중 */}</CountInfo>
+          </InfoImageBox>
         </FlexBox>
       </CardInfo>
     </Wrapper>
@@ -38,24 +69,26 @@ const Wrapper = styled(FlexColumn)`
 const PreviewImageBox = styled(FlexCenter)`
   position: relative;
   height: 250px;
-
   border-radius: 1rem;
+  overflow: hidden;
   border: 1px solid #8e8e8e;
   background-color: ${({ theme }) => theme.color.gray_500};
+  cursor: pointer;
 `;
 
-const FolderAddIcon = styled(FlexCenter)`
+const PreviewImageBoxLayer = styled.div`
   position: absolute;
-  top: 16px;
-  right: 16px;
+  width: 100%;
+  height: 100%;
+  background: linear-gradient(0deg, rgba(0, 0, 0, 0.3) 0%, rgba(255, 255, 255, 0) 30%, rgba(0, 0, 0, 0) 100%);
+`;
 
-  width: 32px;
-  height: 32px;
-
-  border-radius: 4px;
-  background-color: ${({ theme }) => theme.color.gray_700};
-
-  cursor: pointer;
+const PostTitle = styled.span`
+  position: absolute;
+  bottom: 16px;
+  left: 16px;
+  color: white;
+  font-weight: 500;
 `;
 
 const CardInfo = styled(FlexBox)`
@@ -63,11 +96,26 @@ const CardInfo = styled(FlexBox)`
   padding: 16px 0 0;
 `;
 
+const InfoUserBox = styled(FlexCenter)`
+  cursor: pointer;
+`;
+
 const ProfileImageBox = styled.div`
   border-radius: 50%;
   margin-right: 8px;
+  overflow: hidden;
+`;
 
-  cursor: pointer;
+const InfoImageBox = styled(FlexBox)`
+  align-self: center;
+  margin-right: 8px;
+  gap: 2px;
+`;
+
+const CountInfo = styled.span`
+  font-size: 14px;
+  align-self: center;
+  color: #757575;
 `;
 
 export default MainCard;
